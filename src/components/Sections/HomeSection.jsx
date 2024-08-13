ac0769406b89634b91551053fb5149539eea24c0
Lorenzo1799 <lorenzogwada@gmail.com>
2024-08-13 21:51:09 +0200

    animations

--- src/components/Sections/HomeSection.jsx
+++ src/components/Sections/HomeSection.jsx
@@ -22,9 +22,9 @@ const HomeSection = ({ triggerAnimation, scrollToSection }) => {
     <section className="w-screen h-screen flex-shrink-0 snap-start relative">
       <div className="container mx-auto px-4 h-full flex items-center">
         <div className="flex flex-col md:flex-row items-center justify-between w-full">
-          <div className="xs:mt-20 sm:mt-6 md:mt-8 lg:mt-12 xl:mt-16 xs:text-center sm:text-left md:w-1/2 mb-8 md:mb-0 z-10">
+          <div className="xs:mt-20 sm:mt-6 md:mt-8 lg:mt-12 xl:mt-16 xs:text-center sm:text-left md:w-1/2 mb-8 md:mb-0 z-10 md:left-8">
             <h1 className="text-5xl md:text-7xl font-bold mb-4 text-white">
-              Full Stack Developer
+              Frontend Developer
             </h1>
             <p className="text-xl mb-6 text-gray-300">
               Forging robust digital solutions with code and innovation.
@@ -37,7 +37,7 @@ const HomeSection = ({ triggerAnimation, scrollToSection }) => {
             </button>
           </div>
 
-          <div className="md:w-2/3 h-[500px] md:h-[600px] z-0 relative xs:h-[300px] xs:mr-8 xs:w-[300px] xs:mb-4">
+          <div className="md:w-2/3 h-[700px] md:h-[700px] z-0 relative xs:h-[400px] xs:mr-8 xs:w-[500px] xs:mb-4 xs:scale-150 xs:left-6">
             <Spline
               scene="https://prod.spline.design/lPVd1X4CHil3zkUI/scene.splinecode"
             />
