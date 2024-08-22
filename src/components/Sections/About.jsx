a64ff370d17f6b3b5def7ad5802267be24696e78
Lorenzo1799 <lorenzogwada@gmail.com>
2024-08-22 04:32:29 +0200

    aboutText

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -1,50 +1,7 @@
-import Spline from '@splinetool/react-spline';
-import React, { useRef, useEffect } from 'react';
-import gsap from 'gsap';
+import Spline from "@splinetool/react-spline";
+import AboutText from "../UI/AboutText";
 
 const About = () => {
-  const textRef = useRef(null);
-  const lerp = (x, y, a) => x * (1 - a) + y * a;
-
-  function handleMouseMove(event) {
-    const dims = textRef.current.getBoundingClientRect();
-
-    const xstart = dims.x;
-    const xend = xstart + dims.width;
-    const zeroOne = gsap.utils.mapRange(xstart, xend, 0, 1, event.clientX);
-
-    const ystart = dims.y;
-    const yend = ystart + dims.height;
-    const zerotwo = gsap.utils.mapRange(ystart, yend, 0, 1, event.clientY);
-
-    gsap.to(textRef.current, {
-      x: lerp(-20, 20, zeroOne),
-      y: lerp(-30, 30, zerotwo),
-    });
-  }
-
-  function handleMouseLeave() {
-    gsap.to(textRef.current, {
-      x: 0,
-      y: 0,
-    });
-  }
-
-  useEffect(() => {
-    const div = textRef.current;
-    if (div) {
-      div.addEventListener('mousemove', handleMouseMove);
-      div.addEventListener('mouseleave', handleMouseLeave);
-    }
-
-    return () => {
-      if (div) {
-        div.removeEventListener('mousemove', handleMouseMove);
-        div.removeEventListener('mouseleave', handleMouseLeave);
-      }
-    };
-  }, []); 
-
   return (
     <section className="w-screen h-screen flex-shrink-0 snap-start relative flex items-center justify-center">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between w-full">
@@ -54,18 +11,10 @@ const About = () => {
           </div>
         </div>
         <div className="md:w-1/2 text-center md:text-left">
-          <h2 className="text-4xl md:text-5xl font-bold text-white mb-4">About Me</h2>
-          <div ref={textRef} className="px-4 py-2 rounded-2xl transition-colors duration-300 text-white bg-gray-800 font-extrabold font-mono">
-            Hi, I'm a frontend developer with a passion for crafting stunning and performant web experiences. I thrive on tackling complex challenges and continuously learning to stay ahead of the curve.
-          </div>
-          <div className="flex justify-center md:justify-start md:mt-5">
-            <div className="bg-gray-700 text-white px-8 py-4 rounded-full hover:bg-gray-600 transition duration-300 text-lg mr-4">
-              View Resume
-            </div>
-            <div className="bg-transparent border border-gray-600 text-gray-600 px-8 py-4 rounded-full hover:bg-gray-600 hover:text-white transition duration-300 text-lg">
-              Contact Me
-            </div>
-          </div>
+          <h2 className="text-5xl md:text-6xl font-bold text-white mb-8 tracking-tight">
+            About Me
+          </h2>
+          <AboutText />
         </div>
       </div>
     </section>
---
+++ src/components/UI/AboutText.jsx
@@ -0,0 +1,43 @@
+const AboutText = () => {
+  return (
+    <>
+      <div
+        className="
+            px-6 py-4 
+            rounded-2xl 
+            text-white 
+            via-gray-800 to-gray-900
+            shadow-lg shadow-gray-900/50
+            border border-gray-900/20
+            backdrop-blur-sm
+            transform hover:scale-100 transition-all duration-300
+            font-semibold text-lg font-mono
+          "
+      >
+        <p>
+          Hi, I'm a frontend developer with a passion for crafting stunning and
+          performant web experiences.
+        </p>
+      </div>
+      <div
+        className="
+            px-6 py-4 
+            rounded-2xl 
+            text-white 
+            via-gray-800 to-gray-900
+            shadow-lg shadow-gray-900/50
+            border border-gray-900/20
+            backdrop-blur-sm
+            transform hover:scale-100 transition-all duration-300
+            font-semibold text-lg mt-4 font-mono
+          "
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aperiam sit, reiciendis blanditiis ut consectetur quisquam exercitationem a iure voluptas, veniam nesciunt fuga, ad eum eveniet? Incidunt expedita voluptate quo!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutText;
